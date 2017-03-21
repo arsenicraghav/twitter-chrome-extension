ecab0d96ebd29acb113fe9274a455f16ce29bb59
arsenicraghav <mail.raghvendra.kumar@gmail.com>
2017-03-21 17:43:54 +0530

    making the context menu option dynamic depending on content selected

--- app/background.js
+++ app/background.js
@@ -1,9 +1,29 @@
-chrome.contextMenus.create({
-    title: "Tweet it!",
-    contexts:["selection"],
-    onclick: tweetMe
-});
+var contextsList = ["selection", "link", "image", "page"];
 
-function tweetMe(selectedText){
-    chrome.tabs.create({url:"https://twitter.com/intent/tweet?text="+selectedText.selectionText});
+for(var i=0; i<contextsList.length; i++){
+    var currContext = contextsList[i]
+    var contextMenuOption = "Share this " + currContext + " on your twitter profile!";
+    chrome.contextMenus.create({
+        title: contextMenuOption,
+        contexts:[currContext],
+        onclick: tweetMe,
+        id : currContext
+    });
+}
+
+function tweetMe(data){
+    switch(data.menuItemId){
+        case 'selection':
+            chrome.tabs.create({url:"https://twitter.com/intent/tweet?text="+data.selectionText});
+            break;
+        case 'link':
+            chrome.tabs.create({url:"https://twitter.com/intent/tweet?url="+data.linkUrl});
+            break;
+        case 'image':
+            chrome.tabs.create({url:"https://twitter.com/intent/tweet?url="+data.srcUrl});
+            break;
+        case 'page':
+            chrome.tabs.create({url:"https://twitter.com/intent/tweet?text=hardcoded_for_now"});
+            break;
+    }
 }
