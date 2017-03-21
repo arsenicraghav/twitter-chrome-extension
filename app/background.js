46852dd2a2f8c325ebd0ff2a682f4ce3f89a7ed5
arsenicraghav <mail.raghvendra.kumar@gmail.com>
2017-03-21 18:11:03 +0530

    attaching the page url along with page title when sharing a page

--- app/background.js
+++ app/background.js
@@ -23,7 +23,7 @@ function tweetHandler(data,tab){
             chrome.windows.create({url:"https://twitter.com/intent/tweet?url="+encodeURIComponent(data.srcUrl), type: "panel" });
             break;
         case 'page':
-            chrome.windows.create({url:"https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title), type: "panel" });
+            chrome.windows.create({url:"https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+(data.pageUrl), type: "panel" });
             break;
     }
 }
